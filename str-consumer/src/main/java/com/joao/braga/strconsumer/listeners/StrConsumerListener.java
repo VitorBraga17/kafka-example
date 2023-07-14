9cef0735f9126b9ca316ffa676c3e5d11a97dcb1
Vitor-PC <joaovitor1702@gmail.com>
2023-07-14 11:59:42 -0300

    Kafka Consumer

---
+++ str-consumer/src/main/java/com/joao/braga/strconsumer/listeners/StrConsumerListener.java
@@ -0,0 +1,27 @@
+package com.joao.braga.strconsumer.listeners;
+
+import com.joao.braga.strconsumer.custom.StrConsumerCustomListener;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class StrConsumerListener {
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void create(String message) {
+        log.info("CREATE ::: Receive message {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void log(String message) {
+        log.info("LOG ::: Receive message {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-2")
+    public void history(String message) {
+        log.info("HISTORY ::: Receive message {}", message);
+    }
+}
